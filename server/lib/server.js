2d920d55e99136334492cf37503bfe6a72a68432
sulenchy <sulaimanabiodun172@gmail.com>
2017-10-26 17:08:06 +0100

    ft(developer setup the server): server setup features committed

---
+++ server/lib/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+  res.send('Got a POST request');
+});
+
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user');
+});
+
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});

be04db8329ac938ba29e47af64cde89aafddd26a
sulenchy <sulaimanabiodun172@gmail.com>
2017-10-29 16:23:41 +0100

    ft(api):all api endpoints

--- server/lib/server.js
+++ server/lib/server.js
@@ -1,86 +1,91 @@
 import express from 'express';
-import logger from 'morgan';
-
-// recipeResources contains dummy data
-const recipeResources = [
-  {
-    id: 1,
-    title: 'Foo',
-    ingredient: 'seasoning,salt',
-    direction: 'bla bla',
-    upVote: 24,
-    downVote: 2,
-  },
-  {
-    id: 2,
-    title: 'boo',
-    ingredient: 'seasoning,salt',
-    direction: 'bla bla boo',
-    upVote: 24,
-    downVote: 2,
-  },
-
-];
+import bodyParser from 'body-Parser';
+import RecipesController from './controllers/recipes';
+import ReviewsController from './controllers/reviews';
 
 const app = express();
-app.use(logger('dev'));
 
-// post method with string response
-app.post('/', (req, res) => {
-  res.send('Got a POST request');
+const apiRouter = express.Router();
+
+const rc = new RecipesController(apiRouter);
+const rv = new ReviewsController(apiRouter);
+
+// create application/json parser
+
+const jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// GET /api/v1/recipes gets all available recipes
+app.get('/api/v1/recipes', (req, res) => {
+  res.send(rc.getRecipes());
 });
 
-// get method with response from the recipeResource
-app.get('/reciperesources/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  const result = recipeResources.filter(r => r.id === id)[0];
-
-  if (!result) {
-    res.sendStatus(404);
-  } else {
-    res.send(result);
-  }
+app.get('/api/v1/reviews', (req, res) => {
+  res.send(rv.getReviews());
 });
 
-// post method with response from reciperesource (bug: error 500)
-app.post('/reciperesources', (req, res) => {
-  const item = res.body;
-  // const id = recipeResources.length + 1;
-  console.log(item);
-  if (!item.id) {
-    return res.sendStatus(500);
-  }
-
-  recipeResources.push(item);
-  // console.log(id);
-  res.send(`/reciperesources/ ${item.id}`);
+// GET /api/v1/recipes/:id gets a single recipe
+app.get('/api/v1/recipes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  res.send(rc.getSingleRecipe(id));
 });
 
-// get method with /recipeResource path responding from recipeResource
-app.get('/recipeResources', (req, res) => {
-  res.send(recipeResources);
+// GET /api/v1/:id/Reviews/ gets reviews of a single recipe
+app.get('/api/v1/recipes/:id/Reviews/', (req, res) => {
+  const id = parseInt(req.params.id);
+  res.send(rv.getReviewsSingleRecipe(id));
 });
 
-// get method with string response
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// DELETE /api/v1/recipes/:id deletes a recipe from the resource
+app.delete('/api/v1/recipes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  res.send(rc.deleteSingleRecipe(id));
 });
 
-// put method with string response
-app.put('/user', (req, res) => {
-  res.send('Got a PUT request at /user');
+// POST /api/v1/recipes adds new recipe info to the resource
+app.post('/api/v1/recipes', urlencodedParser, (req, res) => {
+  const info = {
+    id: req.body.id,
+    title: req.body.title,
+    description: req.body.description,
+    upVote: req.body.upVote,
+    downVote: req.body.downVote,
+    userId: req.body.userId,
+    dateAdded: new Date(),
+  };
+  res.send(rc.postRecipe(info));
 });
 
-// get method with /about path responding string response
-app.get('/about', (req, res) => {
-  res.send('about');
+// POST /api/v1/recipes adds new review info to the resource
+app.post('/api/v1/recipes/:id/reviews', urlencodedParser, (req, res) => {
+  const info = {
+    id: req.body.id,
+    recipeId: req.body.recipeId,
+    userId: req.body.userId,
+    dateAdded: new Date(),
+    comment: req.body.comment,
+  };
+  console.log(info);
+  res.send(rv.postReview(info));
 });
 
-// delete method with /user path responding a string response
-app.delete('/user', (req, res) => {
-  res.send('Got a DELETE request at /user');
+
+// POST /login gets urlencoded bodies
+app.post('/login', urlencodedParser, (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+  res.send(`welcome, ${req.body.username}`);
+});
+
+// POST /api/users gets JSON bodies
+app.post('/api/v1/users', urlencodedParser, (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+  res.send(req.body);
 });
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('More-recipe server listening on port 3000!');
 });
+
+export default { app };

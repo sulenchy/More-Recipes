972ff04f109277ed04c55ef273d00db4a364cd68
sulenchy <sulaimanabiodun172@gmail.com>
2017-10-30 23:45:27 +0100

    bg(refactor-the api-endpoint):refactoring the api endpoint to make testing easy

---
+++ server/lib/controllers/models/recipes.js
@@ -0,0 +1,32 @@
+const RecipeSchema = [
+  {
+    id: 1,
+    title: 'amala',
+    decription: 'bla bla bla',
+    upVote: 20,
+    downVote: 3,
+    userId: 100,
+    dateAdded: '12/2/2013',
+  },
+  {
+    id: 2,
+    title: 'Rice and stew',
+    decription: 'bla bla bla',
+    upVote: 50,
+    downVote: 3,
+    userId: 100,
+    dateAdded: '12/2/2013',
+  },
+  {
+    id: 3,
+    title: 'Okro Soup',
+    decription: 'bla bla bla',
+    upVote: 10,
+    downVote: 3,
+    userId: 100,
+    dateAdded: '12/2/2013',
+  },
+];
+
+
+export default RecipeSchema;

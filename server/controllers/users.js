1bcd977c8c3fe605f53b4ee0dbbc0665951a6494
sulenchy <sulaimanabiodun172@gmail.com>
2017-11-01 21:02:52 +0100

    ft(create-api-endpoint):commits on create api endpoints

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,15 +1,150 @@
 import model from '../models';
-// const Todo = require('../models').Todo;
+import bcrypt from 'bcrypt';
+
 const users = model.Users;
+const recipes = model.recipes;
 
 const post = {
+  // Signup new user
+  /**
+   * @
+   */
   create(req, res) {
     return users
       .create({
         email: req.body.email,
-        password: req.body.password,
+        password: bcrypt.hashSync(req.body.password, 10),
       })
-      .then(todo => res.status(201).send(todo))
+      .then(user => res.status(201).send(user))
+      .catch(error => res.status(400).send(error));
+  },
+  // Requests and return list of users in users table
+  // list(req, res) {
+  //   return users
+  //     .all()
+  //     .then(usersList => res.status(200).send(usersList))
+  //     .catch(error => res.status(400).send(error));
+  // },
+
+  list(req, res) {
+    return users
+      .findAll({
+        include: [{
+          model: recipes,
+          as: 'recipes',
+        }],
+      })
+      .then(varUsers => res.status(200).send(varUsers))
+      .catch(error => res.status(400).send(error));
+  },
+
+  retrieve(req, res) {
+    return users
+      .findById(req.params.userId, {
+        include: [{
+          model: recipes,
+          as: 'recipes',
+        }],
+      })
+      .then((varUsers) => {
+        if (!varUsers) {
+          return res.status(404).send({
+            message: 'User cannot be found',
+          });
+        }
+        return res.status(200).send({
+          message: 'User logged in successfully',
+        });
+      })
+      .catch(error => res.status(400).send(error));
+  },
+  update(req, res) {
+    return users
+      .findById(req.params.userId, {
+        include: [{
+          model: recipes,
+          as: 'recipes',
+        }],
+      })
+      .then((varUsers) => {
+        if (!varUsers) {
+          return res.status(404).send({
+            message: 'User Not Found',
+          });
+        }
+        return varUsers
+          .update({
+            password: req.body.password || varUsers.password,
+          })
+          .then(() => res.status(200).send(varUsers)) // Send back the updated todo.
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
+
+  destroy(req, res) {
+    return users
+      .findById(req.params.userId)
+      .then((varUsers) => {
+        if (!varUsers) {
+          return res.status(400).send({
+            message: 'User Not Found',
+          });
+        }
+        return varUsers
+          .destroy()
+          .then(() => res.status(204).send(`${req.params.userId} user record deleted `))
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
+
+  update(req, res) {
+    return TodoItem
+      .find({
+          where: {
+            id: req.params.todoItemId,
+            todoId: req.params.todoId,
+          },
+        })
+      .then(todoItem => {
+        if (!todoItem) {
+          return res.status(404).send({
+            message: 'TodoItem Not Found',
+          });
+        }
+  
+        return todoItem
+          .update({
+            content: req.body.content || todoItem.content,
+            complete: req.body.complete || todoItem.complete,
+          })
+          .then(updatedTodoItem => res.status(200).send(updatedTodoItem))
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
+  
+  destroy(req, res) {
+    return TodoItem
+      .find({
+          where: {
+            id: req.params.todoItemId,
+            todoId: req.params.todoId,
+          },
+        })
+      .then(todoItem => {
+        if (!todoItem) {
+          return res.status(404).send({
+            message: 'TodoItem Not Found',
+          });
+        }
+  
+        return todoItem
+          .destroy()
+          .then(() => res.status(204).send())
+          .catch(error => res.status(400).send(error));
+      })
       .catch(error => res.status(400).send(error));
   },
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,28 @@
 import usersController from '../controllers/users';
-
-// const todosController = require('../controllers').todos;
+import recipesController from '../controllers/recipes';
 
 const routes = (app) => {
-  app.get('/api/v1/users', (req, res) => res.status(200).send({
-    message: 'Welcome to the Todos API!',
+  app.get('/api/v1/', (req, res) => res.status(200).send({
+    message: 'Welcome to More Recipe API!',
   }));
 
-  app.post('/api/v1/users', usersController.create);
+  // API endpoints for users to create accounts
+  app.post('/api/v1/users/signup', usersController.create);
+  // API endpoints to list recipes
+  app.get('/api/v1/recipes', recipesController.list);
+  // An API route that allows authenticated user to add a recipe
+  app.post('/api/v1/recipes/:userId/recipes', recipesController.create);
+
+  // Api route to allow users to sigin
+  app.get('/api/v1/users/:userId', usersController.retrieve);
+  app.put('/api/v1/users/:userId', usersController.update);
+  app.delete('/api/v1/users/:userId', usersController.destroy);
+  // An API route that allows authenticated user to modify a recipe they added
+  app.put('/api/v1/users/:userId/recipes/:recipeId', recipesController.update);
+  // An API route that allows authenticated user to delete a recipe they added
+  app.delete('/api/v1/users/:userId/recipes/:recipeId', recipesController.destroy);
+  // An API route that allows a user to get all the recipes in the application
+  app.get('/api/v1/sort/recipes', recipesController.SortByUpvoteDesc);
 };
 
 export default routes;
